49911b7053c9bffb515ba2ffab32cf047e36aaa0
Nicolae Vartolomei <nvartolomei@gmail.com>
2015-04-15 10:51:24 +0300

    bump zoom-instagram-widget script version identificator

--- class.wpzoom-instagram-widget.php
+++ class.wpzoom-instagram-widget.php
@@ -45,7 +45,7 @@ class Wpzoom_Instagram_Widget extends WP_Widget {
 	 */
 	public function scripts() {
 		wp_enqueue_style( 'zoom-instagram-widget', plugin_dir_url( dirname( __FILE__ ) . '/instagram-widget-by-wpzoom.php' ) . 'css/instagram-widget.css', array(), '20150202' );
-		wp_enqueue_script( 'zoom-instagram-widget', plugin_dir_url( dirname( __FILE__ ) . '/instagram-widget-by-wpzoom.php' ) . 'js/instagram-widget.js', array( 'jquery' ), '20150202' );
+		wp_enqueue_script( 'zoom-instagram-widget', plugin_dir_url( dirname( __FILE__ ) . '/instagram-widget-by-wpzoom.php' ) . 'js/instagram-widget.js', array( 'jquery' ), '20150415' );
 	}
 
 	/**
