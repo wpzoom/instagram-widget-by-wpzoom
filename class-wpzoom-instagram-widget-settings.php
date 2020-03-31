b1e61c0c8ea5d8b216208ecff50d5425a96f5bd3
llemurya <llemurya@gmail.com>
2020-03-31 21:27:49 +0300

    set production redirect url

--- class-wpzoom-instagram-widget-settings.php
+++ class-wpzoom-instagram-widget-settings.php
@@ -190,8 +190,7 @@ class Wpzoom_Instagram_Widget_Settings {
 
 		$oauth_url = add_query_arg( [
 			'client_id'     => '1242932982579434',
-			//'redirect_uri'  => 'https://wpzoom.com/instagram-auth/',
-			'redirect_uri'  => 'https://edd.dev/instagram-auth/',
+			'redirect_uri'  => 'https://wpzoom.com/instagram-auth/',
 			'scope'         => 'user_profile,user_media',
 			'response_type' => 'code',
 			'state'         => base64_encode( admin_url( 'options-general.php?page=wpzoom-instagram-widget' ) )
