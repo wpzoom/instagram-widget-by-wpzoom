ca316a4edf0eb34a98ec8860b94c0762325b161a
Pavel Ciorici <cioricip@gmail.com>
2020-06-12 17:27:41 +0200

    some changes to the message

--- instagram-widget-by-wpzoom.php
+++ instagram-widget-by-wpzoom.php
@@ -57,7 +57,7 @@ function wpzoom_instagram_legacy_api_admin_notice() {
 	/* Check that the user hasn't already clicked to ignore the message */
 	if ( ! get_user_meta($user_id, 'wpzoom_instagram_legacy_api_admin_notice') && $request_type === 'with-access-token') {
 		echo '<div class="error notice" style="position:relative"><p>';
-		printf(__('You’re using an <strong>outdated method</strong> to display your Instagram feed. This method will be <a href="https://www.instagram.com/developer/">deprecated soon</a>, please go to the <a href="options-general.php?page=wpzoom-instagram-widget">Settings page</a> and <strong>switch</strong> to the new method.<a style="text-decoration: none" class="notice-dismiss" href="%1$s"></a>'), '?wpzoom_instagram_legacy_api_admin_notice=0');
+		printf(__('You’re using an <strong>outdated method</strong> to display your <strong>Instagram feed</strong>. This method will be <a target="_blank" href="https://www.instagram.com/developer/">deprecated</a> on <strong>June 29, 2020</strong>, and we recommend go to the <a href="options-general.php?page=wpzoom-instagram-widget">Settings page</a> and <strong>switch</strong> to the new method.<a style="text-decoration: none" class="notice-dismiss" href="%1$s"></a>'), '?wpzoom_instagram_legacy_api_admin_notice=0');
 		echo "</p></div>";
 	}
 }
