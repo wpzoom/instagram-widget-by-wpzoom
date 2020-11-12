6d2690260c054776ec663cb2fbf4e35db0bf8e8c
llemurya <llemurya@gmail.com>
2020-11-12 17:31:01 +0200

    isolate enqueue scripts only if widget is rendered in current page

--- class-wpzoom-instagram-widget.php
+++ class-wpzoom-instagram-widget.php
@@ -47,7 +47,12 @@ class Wpzoom_Instagram_Widget extends WP_Widget {
 
 		$this->api = Wpzoom_Instagram_Widget_API::getInstance();
 
-		add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
+		if ( is_active_widget( false, false, 'wpzoom_instagram_widget' ) ) {
+
+			add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
+
+		}
+
 	}
 
 	/**
