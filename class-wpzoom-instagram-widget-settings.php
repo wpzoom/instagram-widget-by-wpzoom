17e6e73c145cc84967b0d478621b835d78f36579
Alexei Samarschi <Samarschi@gmail.com>
2022-06-28 10:49:49 +0200

    Update class-wpzoom-instagram-widget-settings.php
    Hide instagram widget feeds for any other users except admin

--- class-wpzoom-instagram-widget-settings.php
+++ class-wpzoom-instagram-widget-settings.php
@@ -140,6 +140,11 @@ class WPZOOM_Instagram_Widget_Settings {
 	}
 
 	public function init() {
+
+		if( !current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
 		register_post_type(
 			'wpz-insta_user',
 			array(
@@ -363,7 +368,7 @@ class WPZOOM_Instagram_Widget_Settings {
 			}
 		} );
 
-		if ( current_user_can( 'edit_posts' ) && isset( $_GET['wpz-insta-widget-preview'] ) ) {
+		if ( current_user_can( 'manage_options' ) && isset( $_GET['wpz-insta-widget-preview'] ) ) {
 			remove_action( 'admin_enqueue_scripts', 'wp_auth_check_load' );
 			remove_filter( 'heartbeat_send', 'wp_auth_check' );
 			remove_filter( 'heartbeat_nopriv_send', 'wp_auth_check' );
