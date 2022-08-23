13832222e6dc587e8e239ef5b6ea97654b2bb6b4
Pavel Ciorici <cioricip@gmail.com>
2022-08-23 10:20:53 +0200

    fix conflict with swiper from elementor

--- dist/styles/frontend/block.asset.php
+++ dist/styles/frontend/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f003683d7cbcd70fbc645d75d589759d');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'c41ae9cb1f452e88b030c8d0256f5f2d');
