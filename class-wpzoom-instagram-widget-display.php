2b4e112bdedb41c33f08ebb8298a15c2da6fc312
Alexus450 <Samarschi@gmail.com>
2025-03-17 11:03:02 +0200

    css fixes

--- class-wpzoom-instagram-widget-display.php
+++ class-wpzoom-instagram-widget-display.php
@@ -590,7 +590,7 @@ class Wpzoom_Instagram_Widget_Display {
 						$output .= '<a class="zoom-instagram-link" data-src="' . $src . '" data-mfp-src="' . $media_id . '" href="' . $link . '" target="_blank" rel="noopener nofollow" title="' . $alt . '">';
 						$output .= '<div class="hover-controls">';
 							if ( $show_likes && ! empty( $likes ) ) {
-								$output .= '<span class="zoom-instagram-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" id="heart-outline">
+								$output .= '<span class="zoom-instagram-icon icon-heart-outline"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" id="heart-outline">
   <path fill="none" stroke="#ffffff" stroke-width="2" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
 </svg></span>';
 								$output .= '<span class="counter">' . self::format_number( $likes ) . '</span>';
--- dist/styles/frontend/index.asset.php
+++ dist/styles/frontend/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'dc29b588a78676d92e8f');
+<?php return array('dependencies' => array(), 'version' => 'f27eef6c86f655fc09fa');
